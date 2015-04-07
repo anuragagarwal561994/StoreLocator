2f5ab063b18799db2c3d75e9a12f78172130018a
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-04-07 08:29:23 +0530

    Adds a check on do not proceed further before filling up user information

--- app/src/main/java/com/storelocator/altaiezior/SplashScreen.java
+++ app/src/main/java/com/storelocator/altaiezior/SplashScreen.java
@@ -11,18 +11,24 @@ import android.view.MenuItem;
 
 public class SplashScreen extends Activity {
 
-    public static final String PREFERENCE_NAME = "Login";
+    public static final String LOGIN_PREFERENCE_NAME = "Login";
+    public static final String USER_PROFILE_PREFERENCE_NAME = "UserProfile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        final SharedPreferences loginPreference = getSharedPreferences(PREFERENCE_NAME,  0);
+        final SharedPreferences loginPreference = getSharedPreferences(LOGIN_PREFERENCE_NAME, 0);
+        final SharedPreferences userProfilePreference = getSharedPreferences(USER_PROFILE_PREFERENCE_NAME, 0);
 
         final Intent nextActivity;
         if(loginPreference.getBoolean("loggedIn", false))
-            nextActivity = new Intent(this, MainActivity.class);
+            if(userProfilePreference.getString("First Name", "").isEmpty() &&
+                    userProfilePreference.getString("Last Name", "").isEmpty())
+                nextActivity = new Intent(this, UserDetail.class);
+            else
+                nextActivity = new Intent(this, MainActivity.class);
         else
             nextActivity = new Intent(this, LoginActivity.class);
 
