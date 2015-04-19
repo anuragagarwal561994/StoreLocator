52e5b70bca3256100d2b2b3cee3c762c0c505ef2
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-04-20 00:55:09 +0530

    Creates a seperate class for login response enum

---
+++ app/src/main/java/com/storelocator/altaiezior/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.storelocator.altaiezior;
+
+/**
+ * Created by altaiezior on 20/4/15.
+ */
+public enum LoginResponse {
+    NONE,
+    LOGIN,
+    NOT_FOUND,
+    WRONG_PASSWORD,
+    INTERRUPTED
+}
